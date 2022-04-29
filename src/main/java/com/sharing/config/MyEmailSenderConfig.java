b4d57ff5af26e8b1a1fb6b5b574f055079b7dd8c
lifusheng520 <1992968196@qq.com>
2022-04-29 10:26:17 +0800

    times:27

--- src/main/java/com/sharing/config/MyEmailSenderConfig.java
+++ src/main/java/com/sharing/config/MyEmailSenderConfig.java
@@ -2,7 +2,6 @@ package com.sharing.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
